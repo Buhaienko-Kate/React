d30df770eec07c12e8b27ed47f39abe6e4cb1934
Andrei <59658838+DvijIt@users.noreply.github.com>
2021-10-04 17:11:56 +0300

    Update UserForm.jsx

--- Lesson10/task2/src/UserForm.jsx
+++ Lesson10/task2/src/UserForm.jsx
@@ -1,34 +1,13 @@
 import React from 'react';
 
-// class UserForm extends React.Component {
-//   //   handleChange = event => {
-//   //     this.props.handleChange(event);
-//   //   };
-
-//   render() {
-//     return (
-//       <form className="user-form">
-//         <input
-//           type="text"
-//           name="firstName"
-//           className="user-form__input"
-//           value={this.props.userData.firstName}
-//           onChange={this.props.handleChange}
-//         />
-//         <input
-//           type="text"
-//           name="lastName"
-//           className="user-form__input"
-//           value={this.props.userData.lastName}
-//           onChange={this.props.handleChange}
-//         />
-//       </form>
-//     );
-//   }
-// }
-// export default UserForm;
-
 const UserForm = ({ userData, handleChange }) => {
+  
+  const handleOnChange = event => {
+    const { name, value } = event.target;
+    
+    handleChange({...userdata, [name]: value});
+  }
+  
   return (
     <form className="user-form">
       <input
@@ -36,14 +15,14 @@ const UserForm = ({ userData, handleChange }) => {
         name="firstName"
         className="user-form__input"
         value={userData.firstName}
-        onChange={handleChange}
+        onChange={handleOnChange}
       />
       <input
         type="text"
         name="lastName"
         className="user-form__input"
         value={userData.lastName}
-        onChange={handleChange}
+        onChange={handleOnChange}
       />
     </form>
   );
