5717b0ebe9ffbac2bcda7e95a3d7450454d0b87c
Buhaienko-Kate <buguzia89@gmail.com>
2021-10-05 11:42:41 +0300

    change task3

--- Lesson11/task3/src/App.jsx
+++ Lesson11/task3/src/App.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import Expand from './Expand.jsx';
 
 class App extends React.Component {
-  state = {
-    isOpen: false,
-  };
+  // state = {
+  //   isOpen: false,
+  // };
 
-  clickDialog = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  };
+  // clickDialog = () => {
+  //   this.setState({
+  //     isOpen: !this.state.isOpen,
+  //   });
+  // };
 
   render() {
     return (
       <div className="app">
-        <Expand isOpen={this.state.isOpen} onClose={this.clickDialog} title="Recommendation">
+        <Expand title="Recommendation">
           <p>
             Hooks are a new addition in React 16.8. They let you use state and other React features
             without writing a class.
--- Lesson11/task3/src/Expand.jsx
+++ Lesson11/task3/src/Expand.jsx
@@ -3,35 +3,77 @@ import PropTypes from 'prop-types';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const Expand = ({ title, children, onClose, isOpen }) => {
-  // console.log(isOpen);
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={onClose}>
-          {isOpen ? (
-            <i className="fas fa-chevron-up">∧</i>
-          ) : (
-            <i className="fas fa-chevron-down">∨</i>
-          )}
-          {/* {isOpen ? (
+class Expand extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  clickDialog = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+  render() {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.clickDialog}>
+            {this.state.isOpen ? (
+              <i className="fas fa-chevron-up">∧</i>
+            ) : (
+              <i className="fas fa-chevron-down">∨</i>
+            )}
+            {/* {isOpen ? (
             <FontAwesomeIcon icon={faChevronUp} />
           ) : (
             <FontAwesomeIcon icon={faChevronDown} />
           )} */}
-        </button>
+          </button>
+        </div>
+        {this.state.isOpen && <div className="expand__content">{this.props.children}</div>}
       </div>
-      {isOpen && <div className="expand__content">{children}</div>}
-    </div>
-  );
-};
+    );
+  }
+}
+
+// const Expand = ({ title, children }) => {
+//   // console.log(isOpen);
+//   state = {
+//     isOpen: false,
+//   };
+//   clickDialog = () => {
+//     this.setState({
+//       isOpen: !this.state.isOpen,
+//     });
+//   };
+//   return (
+//     <div className="expand border">
+//       <div className="expand__header">
+//         <span className="expand__title">{title}</span>
+//         <button className="expand__toggle-btn" onClick={this.clickDialog}>
+//           {this.state.isOpen ? (
+//             <i className="fas fa-chevron-up">∧</i>
+//           ) : (
+//             <i className="fas fa-chevron-down">∨</i>
+//           )}
+//           {/* {isOpen ? (
+//             <FontAwesomeIcon icon={faChevronUp} />
+//           ) : (
+//             <FontAwesomeIcon icon={faChevronDown} />
+//           )} */}
+//         </button>
+//       </div>
+//       {this.state.isOpen && <div className="expand__content">{children}</div>}
+//     </div>
+//   );
+// };
 
 Expand.propTypes = {
   isOpen: PropTypes.bool,
   children: PropTypes.element.isRequired,
   title: PropTypes.string,
-  onClose: PropTypes.func.isRequired,
+  // onClose: PropTypes.func.isRequired,
 };
 Expand.defaultProps = {
   isOpen: false,
